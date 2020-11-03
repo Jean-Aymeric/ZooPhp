89289f9a7fff07b5a1a8817719288628149c6376
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2020-11-03 15:02:18 +0100

    Gorilla speak "HOU HOU !!!"

--- Gorilla.php
+++ Gorilla.php
@@ -1,7 +1,8 @@
 <?php
 include_once "Ape.php";
 
-class Gorilla extends Ape
-{
-
+class Gorilla extends Ape {
+    public function speak(): void {
+        print(parent::getName() . " : HOU HOU !!!\n");
+    }
 }
